fa48fe1c52ce1bff4fb5e03945ea5563d2d577eb
pimp13 <pimp.puma.13@gmail.com>
2026-09-14 16:24:21 +0330

    add auth service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
+import { AuthModule } from './api/v1/auth/auth.module.js';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
