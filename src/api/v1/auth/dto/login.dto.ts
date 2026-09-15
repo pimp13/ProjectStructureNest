a554dfc7453bc1810f9e6bb7371af0a95e63afba
pimp13 <pimp.puma.13@gmail.com>
2026-09-15 16:03:17 +0330

    use api property for show dto object in swagger docs

--- src/api/v1/auth/dto/login.dto.ts
+++ src/api/v1/auth/dto/login.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty()
   @IsEmail({}, { message: 'ایمیل معتبر نیست' })
   @IsNotEmpty()
-  email: string;
+  email!: string;
 
+  @ApiProperty()
   @IsString()
   @MinLength(8, { message: 'رمز عبور باید حداقل ۸ کاراکتر باشد' })
-  password: string;
+  password!: string;
 }
