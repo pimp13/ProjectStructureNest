6ee4c59001ea56f79ae1b066dec55f6c3d3449d9
pimp13 <pimp.puma.13@gmail.com>
2026-09-15 15:26:41 +0330

    using cache az redis

--- src/api/v1/users/users.service.ts
+++ src/api/v1/users/users.service.ts
@@ -1,18 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
 import { PrismaService } from '../../../prisma/prisma.service.js';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import type { Cache } from 'cache-manager';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
+  ) {}
 
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async remember<T>(
+    key: string,
+    ttl: number,
+    callback: () => Promise<T>,
+  ): Promise<T> {
+    const cached = await this.cacheManager.get<T>(key);
+    if (cached) {
+      return cached;
+    }
+
+    const result = await callback();
+    await this.cacheManager.set(key, result, ttl);
+    return result;
+  }
+
+  async findAll() {
+    const cacheKey = 'users.list';
+    const cachedUsers = await this.cacheManager.get(cacheKey);
+    if (cachedUsers) {
+      return cachedUsers;
+    }
+
+    const users = await this.prisma.user.findMany({
+      orderBy: { createdAt: 'desc' },
+    });
+
+    await this.cacheManager.set(cacheKey, users, 24 * 60 * 60 * 1000);
+
+    return users;
+    // await this.cacheManager.del('users.list');
+    // return 'cache clear';
   }
 
   async findById(id: number) {
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,25 @@ import { PrismaModule } from './prisma/prisma.module.js';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './api/v1/auth/auth.module.js';
 import { UsersModule } from './api/v1/users/users.module.js';
+import { CacheModule } from '@nestjs/cache-manager';
+import { createKeyv } from '@keyv/redis';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true, // تا در همه ماژول‌ها در دسترس باشد
+      useFactory: async () => {
+        return {
+          stores: [
+            createKeyv('redis://localhost:6379'), // آدرس Redis
+          ],
+          ttl: 24 * 60 * 60 * 1000, // زمان پیش‌فرض کش (میلی‌ثانیه) - اینجا ۱ ساعت
+        };
+      },
     }),
     PrismaModule,
     AuthModule,
